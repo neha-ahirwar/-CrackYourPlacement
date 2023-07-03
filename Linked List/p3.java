a1263a1e799df028f86b58b7566c863c076cd1e7
Neha Ahirwar <95342496+neha-ahirwar@users.noreply.github.com>
2023-07-03 21:42:52 +0530

    Create p3.java

---
+++ Linked List/p3.java
@@ -0,0 +1,19 @@
+//I could do this by finding the length of the linked list or by reversing the linked list
+
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+
+        int ans=0;
+        ListNode* temp= head;
+        while(temp!=NULL)
+        {
+            ans*=2;
+            ans+=(temp->val);
+            temp= temp->next;
+        }
+
+        return ans;
+        
+    }
+};
